6cd62e49e8fc19a8b6bea36c926a13bda5b2d51c
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-10 21:20:41 +0400

    Enable Firebase Storage for Coder Hub image assets

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyC9VtSkZlLSyIR2REzy3WqHwSk561NgVQU',
@@ -15,4 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export default app;
